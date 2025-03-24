2bae2ccf90ec6ba1b8f17115c8298f772bcaa950
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-03-24 10:58:50 +0200

    Initial commit

---
+++ src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/JagatudKalendriRakendusApplication.java
@@ -0,0 +1,13 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagatudKalendriRakendusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JagatudKalendriRakendusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/f4lt3ring/jagatudkalendrirakendus/JagatudKalendriRakendusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagatudKalendriRakendusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
