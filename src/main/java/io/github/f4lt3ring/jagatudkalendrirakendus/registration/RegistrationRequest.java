c89adb4e2246f43c9dcf9fad04e302d25d45193d
f4lt3ring <57318281+f4lt3ring@users.noreply.github.com>
2025-04-06 18:15:33 +0300

    Added basic Registration classes

---
+++ src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/registration/RegistrationRequest.java
@@ -0,0 +1,18 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+
+  private final String firstName;
+  private final String lastName;
+  private final String email;
+  private final String password;
+}
---
+++ src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/registration/RegistrationService.java
@@ -0,0 +1,11 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+  public String register(RegistrationRequest request) {
+    return "Registration successful";
+  }
+}
