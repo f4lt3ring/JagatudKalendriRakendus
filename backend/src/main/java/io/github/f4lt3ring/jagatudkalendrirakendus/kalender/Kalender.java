0a5b85b8c99780e7b7f3d5be11696dc08ca8664b
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-04-07 11:25:39 +0300

    ???

---
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/kalender/Kalender.java
@@ -0,0 +1,61 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus.kalender;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.component.VEvent;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+@Entity
+public class Kalender {
+    @Id @GeneratedValue
+    private Long id;
+    // Loob uue *.ics kalendri põhja
+    private Calendar icalCalendar= new Calendar().withProdId("-//"+id+"//iCal4j 1.0//EN")
+            .withDefaults().getFluentTarget();
+
+    public Kalender() {}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Loob uue terve päeva eventi ja lisab selle kalendrisse
+     * @param eventName - Eventi nimi
+     * @param eventDate - Kuupäev, kuhu event lisatakse
+     * @param eventMonthNr - Kuu number, kuhu event lisatakse
+     */
+    public void createAllDayEvent(String eventName, int eventDate, int eventMonthNr) {
+        eventMonthNr = eventMonthNr-1;
+        java.util.Calendar calendarEventStart = java.util.Calendar.getInstance();
+        calendarEventStart.set(java.util.Calendar.MONTH, eventMonthNr);
+        calendarEventStart.set(java.util.Calendar.DAY_OF_MONTH, eventDate);
+
+        VEvent newAllDayEvent = new VEvent(calendarEventStart.getTime().toInstant(), eventName);
+
+        icalCalendar.add(newAllDayEvent);
+    }
+
+    /**
+     * Loob .ics faili mida saab kasutada kõikides seda kasutavates rakendustes
+     * @param filename - faili nimi
+     * @throws IOException
+     */
+    public void generateCalenderFile(String filename) throws IOException {
+        FileOutputStream fout = new FileOutputStream(filename + ".ics");
+
+        CalendarOutputter outputter = new CalendarOutputter();
+        outputter.output(icalCalendar, fout);
+    }
+
+}
---
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/kalender/KalenderRepository.java
@@ -0,0 +1,6 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus.kalender;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KalenderRepository extends JpaRepository<Kalender, Long> {
+}
