eb53106e829499743d254a3fcd7bc637b12515a9
f4lt3ring <57318281+f4lt3ring@users.noreply.github.com>
2025-04-20 16:26:54 +0300

    created email confirmation sender

---
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/email/EmailSender.java
@@ -0,0 +1,5 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
--- backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/WebSecurityConfig.java
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/WebSecurityConfig.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -53,7 +55,8 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/v*/registration/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults());
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
