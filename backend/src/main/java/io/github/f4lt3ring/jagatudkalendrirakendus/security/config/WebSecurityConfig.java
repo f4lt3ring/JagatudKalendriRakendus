dd9b5c6922995cfae20e73bd339420b88d01c9d8
f4lt3ring <57318281+f4lt3ring@users.noreply.github.com>
2025-04-28 12:18:53 +0300

    websecurityconfig updated

--- backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/WebSecurityConfig.java
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -54,7 +55,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/v*/registration/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
