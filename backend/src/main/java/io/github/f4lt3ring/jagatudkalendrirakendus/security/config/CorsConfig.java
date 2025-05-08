08a44f54d7c2d7d64d4df34c5da0e225f0d4f5e7
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-05-08 15:40:09 +0300

    kalendri lisamine frontendi

---
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package io.github.f4lt3ring.jagatudkalendrirakendus.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class CorsConfig {
+
+/*    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        System.out.println("Config running");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }*/
+}
