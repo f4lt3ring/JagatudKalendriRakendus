b38325dc42c8ab176acd0c0bd3142c3f9a15d6e1
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2025-05-09 19:23:46 +0300

    allow needed headers in cors policy

--- backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/CorsConfig.java
+++ backend/src/main/java/io/github/f4lt3ring/jagatudkalendrirakendus/security/config/CorsConfig.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class CorsConfig {
 
-/*    @Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
-        System.out.println("Config running");
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Accept", "Authorization")
                         .allowCredentials(true);
             }
         };
-    }*/
+    }
 }
